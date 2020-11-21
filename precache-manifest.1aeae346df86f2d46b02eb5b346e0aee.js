cbe899ade17e01ea125e447e3233762fa1bb8484
David Dirks <daviddirks12@gmail.com>
2020-11-21 12:17:56 -0600

    Updates

---
+++ precache-manifest.1aeae346df86f2d46b02eb5b346e0aee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e78c54b5412dfdb67d39821902d908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acf5f625258320cdf247",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4f2ba2c3e63c103c58a8",
+    "url": "/static/css/main.627538d4.chunk.css"
+  },
+  {
+    "revision": "acf5f625258320cdf247",
+    "url": "/static/js/2.2c2c7be9.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.2c2c7be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2ba2c3e63c103c58a8",
+    "url": "/static/js/main.f45ead54.chunk.js"
+  },
+  {
+    "revision": "4e2028ae5b7a5195d0f6",
+    "url": "/static/js/runtime-main.b16d9ede.js"
+  },
+  {
+    "revision": "6ca56adfef2e7a4dfe5e5f8aa6da0f07",
+    "url": "/static/media/Icon.6ca56adf.png"
+  },
+  {
+    "revision": "1a6efb27869b865a80cd09a05ab86f11",
+    "url": "/static/media/avatar.1a6efb27.png"
+  }
+]);
