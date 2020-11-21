53f8d16172a6f077980206d4d6199024a330269d
David Dirks <daviddirks12@gmail.com>
2020-11-21 15:59:28 -0600

    Changes with build for github pages

---
+++ build/precache-manifest.d14d695afefd5727c2d0fd71fc640605.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe9232ba6967066caa7345ccc7c9341",
+    "url": "/wired14.github.io/index.html"
+  },
+  {
+    "revision": "f7f5cab45aacbeaf4c09",
+    "url": "/wired14.github.io/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "380260d2dd3099ac7ebc",
+    "url": "/wired14.github.io/static/css/main.627538d4.chunk.css"
+  },
+  {
+    "revision": "f7f5cab45aacbeaf4c09",
+    "url": "/wired14.github.io/static/js/2.79bfcc2b.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/wired14.github.io/static/js/2.79bfcc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380260d2dd3099ac7ebc",
+    "url": "/wired14.github.io/static/js/main.7875ccd3.chunk.js"
+  },
+  {
+    "revision": "1d749d0309afa8410e06",
+    "url": "/wired14.github.io/static/js/runtime-main.7b866ada.js"
+  },
+  {
+    "revision": "6ca56adfef2e7a4dfe5e5f8aa6da0f07",
+    "url": "/wired14.github.io/static/media/Icon.6ca56adf.png"
+  },
+  {
+    "revision": "1a6efb27869b865a80cd09a05ab86f11",
+    "url": "/wired14.github.io/static/media/avatar.1a6efb27.png"
+  }
+]);
